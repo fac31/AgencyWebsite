50ad3dc7beff40d977fa52e2f44c60ae65fa4b5d
Jess Pates <jesspates@hotmail.co.uk>
2024-03-27 13:59:58 +0000

    burger menu implemented for mobile nav

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,5 @@
+// PAWS ANIMATION
+
 const paws = document.querySelectorAll(".fa-paw");
 
 const loadPaws = function() {
@@ -11,6 +13,8 @@ const loadPaws = function() {
 document.addEventListener("DOMContentLoaded", loadPaws);
 
 
+
+// SECTION SLIDE INTO VIEW
 
 const SlideInSections = document.querySelectorAll(".slide-in-section");
 
@@ -30,3 +34,22 @@ SlideInSections.forEach((section) => {
   sectionObserver.observe(section);
   section.classList.add("section-hidden");
 })
+
+
+
+// BURGER MENU
+
+const burgerMenu = document.querySelector(".mobile--nav-hamburger");
+const burgerMenuTop = document.querySelector(".mobile--nav-hamburger-top");
+const burgerMenuMiddle = document.querySelector(".mobile--nav-hamburger-middle");
+const burgerMenuBottom = document.querySelector(".mobile--nav-hamburger-bottom");
+const mobileNavMenu = document.querySelector(".mobile--nav-menu");
+
+const toggleMenu = function() {
+  burgerMenuTop.classList.toggle("mobile--nav-hamburger-top_active");
+  burgerMenuMiddle.classList.toggle("mobile--nav-hamburger-middle_hidden");
+  burgerMenuBottom.classList.toggle("mobile--nav-hamburger-bottom_active");
+  mobileNavMenu.classList.toggle("mobile--nav-menu_hidden");
+}
+
+burgerMenu.addEventListener("click", toggleMenu);
