76e63e853785ca6f9d0eb5b20f35925ea91303d4
Jess Pates <jesspates@hotmail.co.uk>
2024-03-29 17:11:01 +0000

    index html and js files refactored

--- javascript/home.js
+++ javascript/home.js
@@ -1,8 +1,18 @@
+// CREATE PAWS
+
+export function createPaws(container, num) {
+  for (let i = 1; i <= num; i++) {
+    const paw = document.createElement("i");
+    paw.classList.add("fa-solid", "fa-paw", "home--paw-header", `home--paw-${i}`, "home--paw-hidden");
+    container.appendChild(paw);
+  }
+}
+
+
+
 // PAWS ANIMATION
 
-const paws = document.querySelectorAll(".fa-paw");
-
-const loadPaws = function() {
+export function loadPaws(paws) {
   paws.forEach((paw, index) => {
     setTimeout(() => {
       paw.classList.remove("home--paw-hidden");
@@ -10,46 +20,20 @@ const loadPaws = function() {
   })
 }
 
-document.addEventListener("DOMContentLoaded", loadPaws);
 
 
+// CREATE DOG IMAGES
 
-// SECTION SLIDE INTO VIEW
+export function createDogImages(container, images) {
+  images.forEach(image => {
+    const imageBox = document.createElement("div");
+    const dogImg = document.createElement("img");
+    dogImg.setAttribute("src", image)
 
-const SlideInSections = document.querySelectorAll(".slide-in-section");
+    imageBox.classList.add("home--dog-image__container");
+    dogImg.classList.add("home--dog-image");
 
-function revealSection(entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section-hidden");
-  sectionObserver.unobserve(entry.target);
+    imageBox.append(dogImg)
+    container.append(imageBox);
+  })
 }
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15
-})
-
-SlideInSections.forEach((section) => {
-  sectionObserver.observe(section);
-  section.classList.add("section-hidden");
-})
-
-
-
-// BURGER MENU
-
-const burgerMenu = document.querySelector(".mobile--nav-hamburger");
-const burgerMenuTop = document.querySelector(".mobile--nav-hamburger-top");
-const burgerMenuMiddle = document.querySelector(".mobile--nav-hamburger-middle");
-const burgerMenuBottom = document.querySelector(".mobile--nav-hamburger-bottom");
-const mobileNavMenu = document.querySelector(".mobile--nav-menu");
-
-const toggleMenu = function() {
-  burgerMenuTop.classList.toggle("mobile--nav-hamburger-top_active");
-  burgerMenuMiddle.classList.toggle("mobile--nav-hamburger-middle_hidden");
-  burgerMenuBottom.classList.toggle("mobile--nav-hamburger-bottom_active");
-  mobileNavMenu.classList.toggle("mobile--nav-menu_hidden");
-}
-
-burgerMenu.addEventListener("click", toggleMenu);
